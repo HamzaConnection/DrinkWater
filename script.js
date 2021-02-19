f1d2dbadccd0b8a46c9fe9889f8e6cba3af38ea6
Hamza Laroussi <HamzaConnection@hotmail.com>
2021-02-19 09:55:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('.liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+
+
+
+smallCups.forEach((cup, i) => {
+    cup.addEventListener('click', () => highlightCups(i))
+})
+
+
+
+const highlightCups = (i) => {
+
+    if (smallCups[i].classList.contains('full') && !smallCups[i].nextElementSibling.classList.contains('full')) {
+        i--
+
+    }
+
+    smallCups.forEach((cup, i2) => {
+        if (i2 <= i) {
+            cup.classList.add('full')
+        }
+        else {
+            cup.classList.remove('full')
+        }
+    })
+}
