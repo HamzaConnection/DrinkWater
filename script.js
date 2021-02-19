41549fb34090f77f078b3a246f5e9447ba441969
Hamza Laroussi <HamzaConnection@hotmail.com>
2021-02-19 13:07:03 +0100

    DONE

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const smallCups = document.querySelectorAll('.cup-small')
-const liters = document.getElementById('.liters')
+const liters = document.getElementById('liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
 
-
+updateBigCup()
 
 smallCups.forEach((cup, i) => {
     cup.addEventListener('click', () => highlightCups(i))
@@ -27,4 +27,30 @@ const highlightCups = (i) => {
             cup.classList.remove('full')
         }
     })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0;
+    } else {
+        percentage.style.visibility = 'visible';
+        percentage.style.height = `${fullCups / totalCups * 100}%`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remained.style.visibility = 'hidden'
+        remained.style.height = 0;
+    }
+    else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`
+    }
+
 }
